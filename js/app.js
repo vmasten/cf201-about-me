d90ced5419f179ad6fa93fa8ebb85f53a92f6b59
Vince Masten <vmasten@me.com>
2018-07-11 13:07:44 -0700

    Corrections to app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
-"use strict";
+'use strict';
 
 var howTall, bornInWA, hateRain, seahawksFan, seattleResidence;
 
+/*
 howTall = prompt('How tall am I (in inches)?');
 console.log('howTall', howTall);
 if (howTall < 76) {
@@ -12,52 +13,52 @@ if (howTall < 76) {
   alert('Whoa, I\'m not that tall!');
 }
 
-alert('The following questions require a response of y or n.');
+alert('The following questions require a response of y/n or yes/no.');
 
-bornInWA = prompt('Was I born in Washington State?');
+bornInWA = prompt('Was I born in Washington State?').toUpperCase();
 console.log('bornInWA', bornInWA);
-if (bornInWA.toUpperCase() === 'Y') {
-  alert('That\'s right!')
+if (bornInWA === 'Y' || bornInWA === 'YES') {
+  alert('That\'s right!');
 }
-else if (bornInWA.toUpperCase() === 'N') {
+else if (bornInWA === 'N' || bornInWA === 'NO') {
   alert('Turns out I was born and raised here.');
 }
 else {
-  alert('Please enter y or n.');
+  alert('Please enter y/n or yes/no.');
 }
 
-hateRain = prompt('Since I\'ve lived here all my life, does that mean I hate the rain?');
+hateRain = prompt('Since I\'ve lived here all my life, does that mean I hate the rain?').toUpperCase();
 console.log('hateRain', hateRain);
-if (hateRain.toUpperCase() === 'Y') {
+if (hateRain === 'Y' || hateRain === 'YES') {
   alert('Surprisingly, I don\'t mind the rain.');
 }
-else if (hateRain.toUpperCase() === 'N') {
+else if (hateRain === 'N' || hateRain === 'NO') {
   alert('That\'s right! I find the sound comforting.');
 }
 else {
-  alert('Please enter y or n');
+  alert('Please enter y/n or yes/no');
 }
 
-seahawksFan = prompt('This one\'s probably too easy, but since I was born and raised here, am I a fan of the Seahawks?');
+seahawksFan = prompt('This one\'s probably too easy, but since I was born and raised here, am I a fan of the Seahawks?').toUpperCase();
 console.log('seahawksFan', seahawksFan);
-if (seahawksFan.toUpperCase() === 'Y') {
+if (seahawksFan === 'Y' || seahawksFan === 'YES') {
   alert('I told you that question was too easy!');
 }
-else if (seahawksFan.toUpperCase() === 'N') {
-  alert('I know not every Seattleite is a Seahawks fan, but... try again!')
+else if (seahawksFan === 'N' || seahawksFan === 'NO') {
+  alert('I know not every Seattleite is a Seahawks fan, but... try again!');
 }
 else {
-  alert('Please enter y or n');
+  alert('Please enter y/n or yes/no');
 }
-
-seattleResidence = prompt('Maybe I was born in state, but have I always lived in Seattle?');
+*/
+seattleResidence = prompt('Maybe I was born in state, but have I always lived in Seattle?').toUpperCase();
 console.log('seattleResidence', seattleResidence);
-if (seattleResidence.toUpperCase() === 'Y') {
+if (seattleResidence === 'Y' || seattleResidence === 'YES') {
   alert('Nope! I\'ve lived in the city since 2012, but I grew up in the south sound.');
 }
-else if (seattleResidence.toUpperCase() === 'N') {
+else if (seattleResidence === 'N' || seattleResidence === 'NO') {
   alert('Right! I went to school in Sumner, WA. If you have no idea where that is, you\'re not alone!');
 }
 else {
-  alert('Please enter y or n');
+  alert('Please enter y/n or yes/no');
 }
